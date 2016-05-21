12e58a93d3282b4913ae75057bb4d11ed949b399
DeepansuHarbola <deepanshuharbola@gmail.com>
2016-05-21 19:47:33 +0530

    Update adapter

--- app/src/main/java/com/dh/dhweather/views/ForecastAdapter.java
+++ app/src/main/java/com/dh/dhweather/views/ForecastAdapter.java
@@ -64,40 +64,6 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.MyView
         return objects.size();
     }
 
-    /*@Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
-        if(convertView == null){
-            convertView = layoutInflater.inflate(resource,null);
-            holder = new ViewHolder();
-            holder.today = (TextView) convertView.findViewById(R.id.today);
-            holder.maxTemp = (TextView) convertView.findViewById(R.id.maxTemp);
-            holder.minTemp = (TextView) convertView.findViewById(R.id.minTemp);
-            holder.desc = (TextView) convertView.findViewById(R.id.desc);
-            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
-
-
-
-            convertView.setTag(holder);
-        }
-        else{
-            holder = (ViewHolder)convertView.getTag();
-        }
-        Glide.with(context)
-                .load("http://openweathermap.org/img/w/" + objects.get(position).getIcon() + ".png").into(holder.icon);
-        SimpleDateFormat simpleDateFormat= new SimpleDateFormat("EEEE", Locale.getDefault());
-        String todayIs = simpleDateFormat.format(new Date(objects.get(position).getDay() * 1000));
-        DateFormat df = DateFormat.getDateInstance();
-        String date = df.format(new Date(objects.get(position).getDay() * 1000));
-        holder.today.setText(todayIs+" "+date);
-        holder.maxTemp.setText(String.format("%.2f", objects.get(position).getMaxTemp())+"\u00B0");
-        holder.minTemp.setText(String.format("%.2f",objects.get(position).getMinTemp())+"\u00B0");
-        holder.desc.setText(objects.get(position).getDescription());
-
-        return convertView;
-    }*/
-
-
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView today;
         public TextView maxTemp;
