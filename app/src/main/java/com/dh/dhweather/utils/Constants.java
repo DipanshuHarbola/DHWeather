4ae265289b714621bd7fe8540f6f9ae084f88153
DeepanshuHarbola <deepanshuharbola@gmail.com>
2018-01-16 08:12:32 +0530

    remove constant api key

--- app/src/main/java/com/dh/dhweather/utils/Constants.java
+++ app/src/main/java/com/dh/dhweather/utils/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String KEY_LOCATION = "location";
     public static final String KEY_DEFAULT_LOCATION = "New Delhi,IN"; // default location
     public static final String KEY_TEMP_UNIT = "metric"; // celsius
-    public static final String KEY_APP_ID = "66799862bdef1dec478b674374b90f6b";
+    public static final String KEY_APP_ID = "Your API Key";
     public static final String KEY_RESULT_MODE = "json"; // other is xml
     public static final String KEY_RESULT_COUNT = "7"; // max is 16
 }
